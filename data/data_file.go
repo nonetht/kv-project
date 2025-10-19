e890522ba35269bbf4d85199855a5842c0b1cd05
yang <94279769+nonetht@users.noreply.github.com>
2025-10-19 12:19:40 +0800

    add new method for readNBytes and implement ReadLogRecord method

--- data/data_file.go
+++ data/data_file.go
@@ -3,6 +3,7 @@ package data
 import (
 	"bitcask-go/fio"
 	"fmt"
+	"io"
 	"path/filepath"
 )
 
@@ -36,7 +37,23 @@ func OpenDataFile(dirPath string, fileId uint32) (*DataFile, error) {
 // Ps. 为什么LogRecord来要记录 key，value 的长度呢？
 // 先根据 offset 读取 header 部分，随后在拿到key，value长度之后，根据key，value的长度来读取存储的具体key，value信息
 func (df *DataFile) ReadLogRecord(offset int64) (*LogRecord, int64, error) {
-	return nil, 0, nil
+	headerBuf, err := df.readBytes(maxLogRecordSize, offset)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	head, err := DncodeLogRecordHeader(headerBuf)
+	if head == nil {
+		// 如果读取到的 header 为空的话，则说明已经读取完毕，应该返回一个 EOF
+		return nil, 0, io.EOF
+	}
+
+	if head.crc == 0 && head.keySize == 0 && head.valueSize == 0 {
+		return nil, 0, io.EOF
+	}
+
+	keySize, valueSize := head.keySize, head.valueSize
+
 }
 
 // Sync 貌似是数据持久化方法，就是将数据持久化
@@ -57,3 +74,15 @@ func (df *DataFile) Write(buf []byte) error {
 //func (df *DataFile) Close() error {
 //	return nil
 //}
+
+// 指定读取多少字节，调用IOManager读取对应数据，并返回一个字节数组
+// 这里他犯了错误，应该指定返回类型就够了，还要指定返回名称吗？
+func (df *DataFile) readNBytes(n int64, offset int64) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := df.IoManager.Read(b, offset)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
