f97710e6d099f697e04f146b0d7e71a3e7e1ff8c
yang <94279769+nonetht@users.noreply.github.com>
2025-10-30 12:25:24 +0800

    added indexerType, so user could choose Btree or ART

--- setup.go
+++ setup.go
@@ -1,7 +1,20 @@
 package bitcask_go
 
+import "bitcask-go/index"
+
 type Setup struct {
 	DirPath      string // 数据库数据目录
 	DataFileSize int64
 	SyncWrites   bool
+	IndexType    index.IndexType
 }
+
+type IndexerType = int8
+
+const (
+	// BTree 索引
+	BTree IndexerType = iota + 1
+
+	// ART Adpative Radix Tree
+	ART
+)
