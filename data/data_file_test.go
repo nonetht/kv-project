dcd887b881fde1ad50673d21811f02a1de6050b5
yang <94279769+nonetht@users.noreply.github.com>
2025-12-03 17:36:09 +0800

    added unit test

--- data/data_file_test.go
+++ data/data_file_test.go
@@ -1,7 +1,95 @@
 package data
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestOpenDataFile(t *testing.T) {
+	// 这样可以创建一个临时目录用于测试，避免污染系统的/tmp目录
+	// 将 os.TempDir() 修改为 t.TempDir() 可以自动创建一个专属于本次测试的临时目录
+	tempDir := t.TempDir()
+	fmt.Println("tempDir:", tempDir)
 
+	dataFile1, err := OpenDataFile(tempDir, 0)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile1)
+
+	dataFile2, err := OpenDataFile(tempDir, 111)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile2)
+
+	dataFile3, err := OpenDataFile(tempDir, 111)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile3)
+}
+
+func TestDataFile_Write(t *testing.T) {
+	dataFile, err := OpenDataFile(os.TempDir(), 111)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile)
+
+	err = dataFile.Write([]byte("aaa")) // 写入一条数据
+	assert.Nil(t, err)
+
+	err = dataFile.Write([]byte("bbb")) // 写入一条数据
+	assert.Nil(t, err)
+
+	err = dataFile.Write([]byte("ccc")) // 写入一条数据
+	assert.Nil(t, err)
+}
+
+func TestDataFile_Close(t *testing.T) {
+	dataFile, err := OpenDataFile(os.TempDir(), 123)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile)
+
+	err = dataFile.Write([]byte("aaa")) // 写入一条数据
+	assert.Nil(t, err)
+
+	err = dataFile.Close()
+	assert.Nil(t, err)
+}
+
+func TestDataFile_Sync(t *testing.T) {
+	dataFile, err := OpenDataFile(os.TempDir(), 502)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile)
+
+	err = dataFile.Write([]byte("aaa")) // 写入一条数据
+	assert.Nil(t, err)
+
+	err = dataFile.Sync()
+	assert.Nil(t, err)
+}
+
+func TestDataFile_ReadLogRecord(t *testing.T) {
+	dataFile, err := OpenDataFile(os.TempDir(), 222)
+	assert.Nil(t, err)
+	assert.NotNil(t, dataFile)
+
+	// 只有一条 LogRecord
+	rec1 := &LogRecord{
+		Key:   []byte("name"),
+		Value: []byte("bitcask-go"),
+		Type:  LogRecordNormal,
+	}
+
+	// 一旦涉及到 DataFile 我就开始有点不太理解...
+	res1, size1 := EncodeLogRecord(rec1)
+	err = dataFile.Write(res1)
+	assert.Nil(t, err)
+
+	// readSize1 就是 LogRecord 的总长度
+	readRec1, readSize1, err := dataFile.ReadLogRecord(0)
+	assert.Nil(t, err)
+	assert.Equal(t, rec1, readRec1)
+	assert.Equal(t, size1, readSize1)
+
+	// 多条 LogRecord 的从不同的位置读取
+
+	// 被删除的数据在数据文件的末尾
 }
