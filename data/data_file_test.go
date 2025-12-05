dde0f7e3bf7d73835bcbfc420b10354773b9f792
yang <94279769+nonetht@users.noreply.github.com>
2025-12-05 12:16:39 +0800

    finish unit test function -- TestDataFile_ReadLogRecord

--- data/data_file_test.go
+++ data/data_file_test.go
@@ -80,17 +80,53 @@ func TestDataFile_ReadLogRecord(t *testing.T) {
 	}
 
 	// 将 rec1 编码，随后写入到 dataFile 之中
+	// 关键就是 dataFile 也是很关键的一环，而 EncodeLogRecord 改变了 dataFile！
 	res1, size1 := EncodeLogRecord(rec1)
 	err = dataFile.Write(res1)
 	assert.Nil(t, err)
+	//t.Log(size1) // 24
+
+	// 声明偏移量 offset
+	var offset int64 = 0
 
 	// readSize1 就是 LogRecord 的总长度
-	readRec1, readSize1, err := dataFile.ReadLogRecord(0) // 随后我们从起始位置（offset=0）开始读取数据
+	readRec1, readSize1, err := dataFile.ReadLogRecord(offset) // 随后我们从起始位置（offset=0）开始读取数据
 	assert.Nil(t, err)
 	assert.Equal(t, rec1, readRec1)
 	assert.Equal(t, size1, readSize1)
+	//t.Log(readSize1) // 24
 
 	// 多条 LogRecord 的从不同的位置读取
+	rec2 := &LogRecord{
+		Key:   []byte("name"),
+		Value: []byte("a new value"),
+	}
+	res2, size2 := EncodeLogRecord(rec2)
+	err = dataFile.Write(res2)
+	assert.Nil(t, err)
 
-	// 被删除的数据在数据文件的末尾
+	//t.Log(size2) // 22
+	offset += readSize1 // 从 rec1 后续开始执行读取
+	readRec2, readSize2, err := dataFile.ReadLogRecord(offset)
+	assert.Nil(t, err)
+	assert.Equal(t, rec2, readRec2)
+	assert.Equal(t, size2, readSize2)
+
+	// 待删除类型的数据
+	rec3 := &LogRecord{
+		Key:   []byte("1"),
+		Value: []byte("1"),
+		Type:  LogRecordDeleted,
+	}
+
+	res3, size3 := EncodeLogRecord(rec3) // 解码得到的字节数组
+	err = dataFile.Write(res3)           // 写入操作，是将字节数组类型写入
+	assert.Nil(t, err)
+	//t.Log(size3) // 9
+
+	offset += readSize2
+	readRec3, readSize3, err := dataFile.ReadLogRecord(offset)
+	assert.Nil(t, err)
+	assert.Equal(t, rec3, readRec3)
+	assert.Equal(t, size3, readSize3)
 }
