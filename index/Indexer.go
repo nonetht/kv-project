aa1fcb4b6a494b6df2163a22c561d13b04b84e94
yang <94279769+nonetht@users.noreply.github.com>
2025-12-08 12:15:58 +0800

    add common index interface

--- index/Indexer.go
+++ index/Indexer.go
@@ -46,3 +46,27 @@ func (ai *Item) Less(bi btree.Item) bool { // Go语言中，方法接收者决�
 	// bi.(*Item): 被称为“类型断言”，相当于：编译器知道bi中存储的有*Item类型的变量，让它(bi 接口)取出来
 	return bytes.Compare(ai.key, bi.(*Item).key) == -1
 }
+
+// Iterator 通用索引迭代器的接口
+type Iterator interface {
+	// Rewind 重新回到迭代器的起点，即第一个数据
+	Rewind()
+
+	// Seek 根据传入的 key 查找到第一个大于等于的 key，根据这个 key 开始遍历
+	Seek(key []byte)
+
+	// Next 跳转到下一个 key
+	Next()
+
+	// Valid 是否有效，即是否已经遍历完了所有的 key，用于退出遍历
+	Valid() bool
+
+	// Key 当前遍历位置的 Key 数据
+	Key() []byte
+
+	// Value 当前遍历位置的 Value 数据
+	Value() *data.LogRecordPos
+
+	// Close 关闭迭代器，释放相应资源
+	Close()
+}
