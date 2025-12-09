7bb2aba26888b74f3c5db0b723deef14b76a5afc
yang <94279769+nonetht@users.noreply.github.com>
2025-12-09 12:36:37 +0800

    added skipToNext method

--- iterator.go
+++ iterator.go
@@ -2,13 +2,14 @@ package bitcask_go
 
 import (
 	"bitcask-go/index"
+	"bytes"
 )
 
 // Iterator 迭代器
 type Iterator struct {
 	indexIter index.Iterator // 索引迭代器
 	db        *DB
-	options   IteratorSetup
+	setup     IteratorSetup
 }
 
 // NewIterator 初始化迭代器
@@ -17,7 +18,7 @@ func (db *DB) NewIterator(opts IteratorSetup) *Iterator {
 	return &Iterator{
 		db:        db,
 		indexIter: indexIter,
-		options:   opts,
+		setup:     opts,
 	}
 }
 
@@ -28,14 +29,18 @@ func (db *DB) NewIterator(opts IteratorSetup) *Iterator {
 // Rewind 重新回到迭代器起点
 func (it *Iterator) Rewind() {
 	it.indexIter.Rewind()
+	it.skipToNext()
 }
 
 func (it *Iterator) Seek(key []byte) {
 	it.indexIter.Seek(key)
+	it.skipToNext()
 }
 
+// Next 跳转到下一个 key
 func (it *Iterator) Next() {
 	it.indexIter.Next()
+	it.skipToNext()
 }
 
 func (it *Iterator) Valid() bool {
@@ -54,13 +59,27 @@ func (it *Iterator) Value() ([]byte, error) {
 	it.db.mu.Lock()
 	defer it.db.mu.RUnlock()
 
-	value, err := it.db.getValueFromPos(logRecordPos)
-	if err != nil {
-		return nil, err
-	}
-	return value, nil
+	return it.db.getValueFromPos(logRecordPos)
 }
 
 func (it *Iterator) Close() {
 	it.indexIter.Close()
+}
+
+// 关于 IteratorSetup.Prefix 相关的内容
+// TODO: 为什么要做这一步呢？为什么要检查 prefix 内容？筛选，将全部前缀为 prefix 的过滤出来。
+func (it *Iterator) skipToNext() {
+	// prefix 长度为0，不需要做任何处理。
+	prefixLen := len(it.setup.Prefix)
+	if prefixLen == 0 {
+		return
+	}
+
+	for ; it.indexIter.Valid(); it.indexIter.Next() {
+		key := it.indexIter.Key()
+		// 如果符合条件，我们就可以将其
+		if prefixLen <= len(key) && bytes.Compare(it.setup.Prefix, key[:prefixLen]) == 0 {
+			break
+		}
+	}
 }
