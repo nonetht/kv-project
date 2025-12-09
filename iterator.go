75694d49a0f82f29152ec6c0b0dfc7dbccf0dbfd
yang <94279769+nonetht@users.noreply.github.com>
2025-12-09 11:46:42 +0800

    added iterator for user to use

--- iterator.go
+++ iterator.go
@@ -1,1 +1,54 @@
 package bitcask_go
+
+import (
+	"bitcask-go/data"
+	"bitcask-go/index"
+)
+
+// Iterator 迭代器
+type Iterator struct {
+	indexIter index.Iterator // 索引迭代器
+	db        *DB
+	options   IteratorSetup
+}
+
+// NewIterator 初始化迭代器
+func (db *DB) NewIterator(opts IteratorSetup) *Iterator {
+	db.index.Iterator(opts.Reverse).Rewind()
+}
+
+// Rewind 重新回到迭代器起点
+func (it *Iterator) Rewind() {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (it *Iterator) Seek(key []byte) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (it *Iterator) Next() {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (it *Iterator) Valid() bool {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (it *Iterator) Key() []byte {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (it *Iterator) Value() *data.LogRecordPos {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (it *Iterator) Close() {
+	//TODO implement me
+	panic("implement me")
+}
